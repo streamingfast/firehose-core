f8580aac54edf85d279fbe343d59c15c54b8fc2d
Matthieu Vachon <matt@streamingfast.io>
2023-07-18 22:59:30 -0400

    Added `tools check forks <forked-blocks-store-url>` to resolve forks longuest segments

--- tools_check.go
+++ tools_check.go
@@ -15,15 +15,26 @@
 package firecore
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/spf13/cobra"
 	"github.com/streamingfast/bstream"
+	"github.com/streamingfast/cli"
 	"github.com/streamingfast/cli/sflags"
+	"github.com/streamingfast/dstore"
 	"github.com/streamingfast/firehose-core/tools"
 )
 
 var toolsCheckCmd = &cobra.Command{Use: "check", Short: "Various checks for deployment, data integrity & debugging"}
+
+var toolsCheckForksCmd = &cobra.Command{
+	Use:   "forks <forked-blocks-store-url>",
+	Short: "Reads all forked blocks you have and print longest linkable segments for each fork",
+	Args:  cobra.ExactArgs(1),
+}
 
 var toolsCheckMergedBlocksCmd = &cobra.Command{
 	// TODO: Not sure, it's now a required thing, but we could probably use the same logic as `start`
@@ -36,12 +47,15 @@ var toolsCheckMergedBlocksCmd = &cobra.Command{
 
 func init() {
 	toolsCmd.AddCommand(toolsCheckCmd)
+	toolsCheckCmd.AddCommand(toolsCheckForksCmd)
 	toolsCheckCmd.AddCommand(toolsCheckMergedBlocksCmd)
 
 	toolsCheckCmd.PersistentFlags().StringP("range", "r", "", "Block range to use for the check")
 
 	toolsCheckMergedBlocksCmd.Flags().BoolP("print-stats", "s", false, "Natively decode each block in the segment and print statistics about it, ensuring it contains the required blocks")
 	toolsCheckMergedBlocksCmd.Flags().BoolP("print-full", "f", false, "Natively decode each block and print the full JSON representation of the block, should be used with a small range only if you don't want to be overwhelmed")
+
+	toolsCheckForksCmd.Flags().Uint64("min-depth", 1, "Only show forks that are at least this deep")
 }
 
 func configureToolsCheckCmd[B Block](chain *Chain[B]) {
@@ -54,6 +68,8 @@ func configureToolsCheckCmd[B Block](chain *Chain[B]) {
 		"s3://<project>/<bucket>/<path>" -f
 		"az://<project>/<bucket>/<path>" -r "10 000 - 1 000 000"
 	`)
+
+	toolsCheckForksCmd.RunE = createToolsCheckForksE(blockPrinter)
 }
 
 func createToolsCheckMergedBlocksE(blockPrinter BlockPrinterFunc) CommandExecutor {
@@ -79,4 +95,80 @@ func createToolsCheckMergedBlocksE(blockPrinter BlockPrinterFunc) CommandExecuto
 			blockPrinter(block, false, os.Stdout)
 		}, printDetails)
 	}
+}
+
+func toolsCheckForksE(cmd *cobra.Command, args []string) error {
+	storeURL := args[0]
+
+	blocksStore, err := dstore.NewDBinStore(storeURL)
+	cli.NoError(err, "unable to create blocks store")
+
+	oneBlockFiles := []*bstream.OneBlockFile{}
+	oneBlockFilesByID := map[string]*bstream.OneBlockFile{}
+	err = blocksStore.Walk(cmd.Context(), "", func(filename string) error {
+		file, err := bstream.NewOneBlockFile(filename)
+		cli.NoError(err, "unable to parse block filename %q", filename)
+
+		oneBlockFiles = append(oneBlockFiles, file)
+		oneBlockFilesByID[file.ID] = file
+		return nil
+	})
+	cli.NoError(err, "unable to walk blocks store")
+
+	if len(oneBlockFiles) == 0 {
+		fmt.Println("No forked blocks found")
+	}
+
+	parentOf := map[string]*bstream.OneBlockFile{}
+	for _, file := range oneBlockFiles {
+		parentOf[file.ID] = oneBlockFilesByID[file.PreviousID]
+	}
+
+	// All forks indexed by their lowest height parent, list is always sorted by height
+	links := map[string][]*bstream.OneBlockFile{}
+	for _, file := range oneBlockFiles {
+		links[file.ID] = []*bstream.OneBlockFile{file}
+	}
+
+	for {
+		changed := false
+		for root, chain := range links {
+			if parent := parentOf[root]; parent != nil {
+				delete(links, root)
+				links[parent.ID] = append([]*bstream.OneBlockFile{parent}, chain...)
+				changed = true
+			}
+
+		}
+		if !changed {
+			break
+		}
+	}
+
+	for _, link := range links {
+		if len(link) < int(sflags.MustGetUint64(cmd, "min-depth")) {
+			continue
+		}
+
+		chain := make([]string, len(link))
+		for i, segment := range link {
+			spaces := strings.Repeat(" ", (i)+2)
+
+			canonical := ""
+			if i == 0 {
+				canonical = " (on chain)"
+			}
+			chain[i] = fmt.Sprintf(spaces+"#%d [%s <= %s%s]", blockNumber(segment.Num), segment.ID, segment.PreviousID, canonical)
+		}
+
+		fmt.Printf("Fork Depth %d\n%s\n\n", len(link), strings.Join(chain, "\n"))
+	}
+
+	return nil
+}
+
+type blockNumber uint64
+
+func (b blockNumber) String() string {
+	return fmt.Sprintf("#%s", humanize.Comma(int64(b)))
 }
