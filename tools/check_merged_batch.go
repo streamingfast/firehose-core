8d721b684de7d52841cb35a77e6180010a74dc17
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-10-26 16:23:09 -0400

    add `tools check merged-blocks-batch`

--- tools/check_merged_batch.go
+++ tools/check_merged_batch.go
@@ -8,18 +8,15 @@ import (
 	"strings"
 
 	"github.com/streamingfast/bstream"
-	"github.com/streamingfast/bstream/forkable"
 	"github.com/streamingfast/dstore"
-	"go.uber.org/zap"
 )
 
-// FlagMergedBlocks will write a list of base-block-numbers to a store, for merged-blocks-files that are broken or missing
+// CheckMergedBlocksBatch will write a list of base-block-numbers to a store, for merged-blocks-files that are broken or missing
 // broken merged-blocks-files are the ones that contain "empty" blocks (no ID) or unlinkable blocks
 // there could be false positives on unlinkable blocks, though
 // output files are like this: 0000123100.broken  0000123500.missing
-func FlagMergedBlocks(
+func CheckMergedBlocksBatch(
 	ctx context.Context,
-	logger *zap.Logger,
 	sourceStoreURL string,
 	destStoreURL string,
 	fileBlockSize uint32,
@@ -43,24 +40,19 @@ func FlagMergedBlocks(
 
 	var firstFilename = fmt.Sprintf("%010d", RoundToBundleStartBlock(uint32(blockRange.Start), fileBlockSize))
 
-	logger.Debug("walking merged blocks", zap.Stringer("block_range", blockRange), zap.String("first_filename", firstFilename))
+	var lastBlockHash string
 	err = blocksStore.WalkFrom(ctx, "", firstFilename, func(filename string) error {
 		if strings.HasSuffix(filename, ".tmp") {
-			logger.Debug("skipping unknown tmp file", zap.String("filename", filename))
 			return nil
 		}
 		match := numberRegex.FindStringSubmatch(filename)
 		if match == nil {
-			logger.Debug("skipping unknown file", zap.String("filename", filename))
 			return nil
 		}
-
-		logger.Debug("received merged blocks", zap.String("filename", filename))
 
 		// should not happen with firstFilename, but leaving in case
 		baseNum, _ := strconv.ParseUint(match[1], 10, 32)
 		if baseNum+uint64(fileBlockSize)-1 < uint64(blockRange.Start) {
-			logger.Debug("base num lower than block range start, quitting", zap.Uint64("base_num", baseNum), zap.Int64("starting_at", blockRange.Start))
 			return nil
 		}
 
@@ -69,22 +61,26 @@ func FlagMergedBlocks(
 		}
 		for expected < baseNum {
 			outputFile := fmt.Sprintf("%010d.missing", expected)
-			logger.Info("found missing file, writing to store", zap.String("output_file", outputFile))
+			fmt.Printf("found missing file %s, writing to store\n", outputFile)
 			destStore.WriteObject(ctx, outputFile, strings.NewReader(""))
 			expected += fileBlockSize64
 		}
 
-		broken, err := checkMergedBlockFileBroken(ctx, blocksStore, filename)
+		broken, lastHash, err := checkMergedBlockFileBroken(ctx, blocksStore, filename, lastBlockHash)
 		if broken {
 			outputFile := fmt.Sprintf("%010d.broken", baseNum)
-			logger.Info("found broken file, writing to store", zap.String("output_file", outputFile))
+			fmt.Printf("found broken file %s, writing to store\n", outputFile)
 			destStore.WriteObject(ctx, outputFile, strings.NewReader(""))
+			lastBlockHash = ""
+		} else {
+			lastBlockHash = lastHash
 		}
+
 		if err != nil {
 			return err
 		}
 
-		if !blockRange.IsClosed() && RoundToBundleEndBlock(uint32(baseNum), fileBlockSize) >= uint32(*blockRange.Stop-1) {
+		if blockRange.IsClosed() && RoundToBundleEndBlock(uint32(baseNum), fileBlockSize) >= uint32(*blockRange.Stop-1) {
 			return dstore.StopIteration
 		}
 		expected = baseNum + fileBlockSize64
@@ -102,23 +98,22 @@ func checkMergedBlockFileBroken(
 	ctx context.Context,
 	store dstore.Store,
 	filename string,
-) (broken bool, err error) {
-
-	tfdb := &trackedForkDB{
-		fdb: forkable.NewForkDB(),
-	}
+	lastBlockHash string,
+) (broken bool, lastHash string, err error) {
+	fmt.Println("checking", filename)
 
 	reader, err := store.OpenObject(ctx, filename)
 	if err != nil {
-		return true, err
+		return true, "", err
 	}
 	defer reader.Close()
 
 	readerFactory, err := bstream.GetBlockReaderFactory.New(reader)
 	if err != nil {
-		return true, err
+		return true, "", err
 	}
 
+	lastHash = lastBlockHash
 	for {
 		var block *bstream.Block
 		block, err = readerFactory.Read()
@@ -138,18 +133,14 @@ func checkMergedBlockFileBroken(
 			return
 		}
 
-		if !tfdb.fdb.HasLIB() {
-			tfdb.fdb.AddLink(block.AsRef(), block.PreviousID(), nil)
-			tfdb.fdb.InitLIB(block)
-			continue
+		if lastHash == "" {
+			lastHash = block.PreviousId
 		}
-
-		tfdb.fdb.AddLink(block.AsRef(), block.PreviousID(), nil)
-		revSeg, _ := tfdb.fdb.ReversibleSegment(block.AsRef())
-		if revSeg == nil {
+		if block.PreviousId != lastHash {
 			broken = true
 			return
 		}
+		lastHash = block.Id
 	}
 
 }
---
+++ tools_checkmergedbatch.go
@@ -0,0 +1,54 @@
+// Copyright 2021 dfuse Platform Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package firecore
+
+import (
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/streamingfast/firehose-core/tools"
+)
+
+var toolsCheckMergedBlocksBatchCmd = &cobra.Command{
+	Use:   "merged-blocks-batch <store-url> <results-store-url> <start> <stop>",
+	Short: "Checks for any holes or unlinkable blocks in merged blocks files, writing the results as files in a results store",
+	Args:  cobra.ExactArgs(4),
+	RunE:  checkMergedBlocksBatchRunE,
+}
+
+func init() {
+	toolsCheckCmd.AddCommand(toolsCheckMergedBlocksBatchCmd)
+}
+
+func checkMergedBlocksBatchRunE(cmd *cobra.Command, args []string) error {
+	storeURL := args[0]
+	resultsStoreURL := args[1]
+	start, err := strconv.ParseUint(args[2], 10, 64)
+	if err != nil {
+		return err
+	}
+	stop, err := strconv.ParseUint(args[3], 10, 64)
+	if err != nil {
+		return err
+	}
+	fileBlockSize := uint32(100)
+
+	blockRange := tools.BlockRange{
+		Start: int64(start),
+		Stop:  &stop,
+	}
+
+	return tools.CheckMergedBlocksBatch(cmd.Context(), storeURL, resultsStoreURL, fileBlockSize, blockRange)
+}
