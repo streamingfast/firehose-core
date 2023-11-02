5e65a249c46f76452b271f5371e31789b2f661b5
Matthieu Vachon <matt@streamingfast.io>
2023-11-02 12:07:30 -0400

    Updated `fire<chain> tools prints merged-blocks` to accept a range and respect single block

--- tools_print_enum.go
+++ tools_print_enum.go
@@ -16,13 +16,18 @@ const (
 	PrintOutputModeText PrintOutputMode = iota
 	// PrintOutputModeJSON is a PrintOutputMode of type JSON.
 	PrintOutputModeJSON
+	// PrintOutputModeJSONL is a PrintOutputMode of type JSONL.
+	PrintOutputModeJSONL
 )
 
-const _PrintOutputModeName = "TextJSON"
+var ErrInvalidPrintOutputMode = fmt.Errorf("not a valid PrintOutputMode, try [%s]", strings.Join(_PrintOutputModeNames, ", "))
+
+const _PrintOutputModeName = "TextJSONJSONL"
 
 var _PrintOutputModeNames = []string{
 	_PrintOutputModeName[0:4],
 	_PrintOutputModeName[4:8],
+	_PrintOutputModeName[8:13],
 }
 
 // PrintOutputModeNames returns a list of possible string values of PrintOutputMode.
@@ -33,8 +38,9 @@ func PrintOutputModeNames() []string {
 }
 
 var _PrintOutputModeMap = map[PrintOutputMode]string{
-	PrintOutputModeText: _PrintOutputModeName[0:4],
-	PrintOutputModeJSON: _PrintOutputModeName[4:8],
+	PrintOutputModeText:  _PrintOutputModeName[0:4],
+	PrintOutputModeJSON:  _PrintOutputModeName[4:8],
+	PrintOutputModeJSONL: _PrintOutputModeName[8:13],
 }
 
 // String implements the Stringer interface.
@@ -45,14 +51,23 @@ func (x PrintOutputMode) String() string {
 	return fmt.Sprintf("PrintOutputMode(%d)", x)
 }
 
-var _PrintOutputModeValue = map[string]PrintOutputMode{
-	_PrintOutputModeName[0:4]:                  PrintOutputModeText,
-	strings.ToLower(_PrintOutputModeName[0:4]): PrintOutputModeText,
-	_PrintOutputModeName[4:8]:                  PrintOutputModeJSON,
-	strings.ToLower(_PrintOutputModeName[4:8]): PrintOutputModeJSON,
+// IsValid provides a quick way to determine if the typed value is
+// part of the allowed enumerated values
+func (x PrintOutputMode) IsValid() bool {
+	_, ok := _PrintOutputModeMap[x]
+	return ok
 }
 
-// ParsePrintOutputMode attempts to convert a string to a PrintOutputMode
+var _PrintOutputModeValue = map[string]PrintOutputMode{
+	_PrintOutputModeName[0:4]:                   PrintOutputModeText,
+	strings.ToLower(_PrintOutputModeName[0:4]):  PrintOutputModeText,
+	_PrintOutputModeName[4:8]:                   PrintOutputModeJSON,
+	strings.ToLower(_PrintOutputModeName[4:8]):  PrintOutputModeJSON,
+	_PrintOutputModeName[8:13]:                  PrintOutputModeJSONL,
+	strings.ToLower(_PrintOutputModeName[8:13]): PrintOutputModeJSONL,
+}
+
+// ParsePrintOutputMode attempts to convert a string to a PrintOutputMode.
 func ParsePrintOutputMode(name string) (PrintOutputMode, error) {
 	if x, ok := _PrintOutputModeValue[name]; ok {
 		return x, nil
@@ -61,15 +76,15 @@ func ParsePrintOutputMode(name string) (PrintOutputMode, error) {
 	if x, ok := _PrintOutputModeValue[strings.ToLower(name)]; ok {
 		return x, nil
 	}
-	return PrintOutputMode(0), fmt.Errorf("%s is not a valid PrintOutputMode, try [%s]", name, strings.Join(_PrintOutputModeNames, ", "))
+	return PrintOutputMode(0), fmt.Errorf("%s is %w", name, ErrInvalidPrintOutputMode)
 }
 
-// MarshalText implements the text marshaller method
+// MarshalText implements the text marshaller method.
 func (x PrintOutputMode) MarshalText() ([]byte, error) {
 	return []byte(x.String()), nil
 }
 
-// UnmarshalText implements the text unmarshaller method
+// UnmarshalText implements the text unmarshaller method.
 func (x *PrintOutputMode) UnmarshalText(text []byte) error {
 	name := string(text)
 	tmp, err := ParsePrintOutputMode(name)
