429831242b97a6f48c8c19fa3cda22d6509141f0
arnaudberger <arnaudberger84@gmail.com>
2024-04-25 09:47:57 -0400

    Renaming block type flag

--- cmd/apps/substreams_tier1.go
+++ cmd/apps/substreams_tier1.go
@@ -50,7 +50,7 @@ func RegisterSubstreamsTier1App[B firecore.Block](chain *firecore.Chain[B], root
 			cmd.Flags().Bool("substreams-tier1-subrequests-insecure", false, "Connect to tier2 without checking certificate validity")
 			cmd.Flags().Bool("substreams-tier1-subrequests-plaintext", true, "Connect to tier2 without client in plaintext mode")
 			cmd.Flags().Int("substreams-tier1-max-subrequests", 4, "number of parallel subrequests that the tier1 can make to the tier2 per request")
-			cmd.Flags().String("block-type", "", "Block type to use for the substreams tier1 (Ex: sf.ethereum.type.v2.Block)")
+			cmd.Flags().String("substreams-tier1-block-type", "", "Block type to use for the substreams tier1 (Ex: sf.ethereum.type.v2.Block)")
 
 			// all substreams
 			registerCommonSubstreamsFlags(cmd)
@@ -90,7 +90,7 @@ func RegisterSubstreamsTier1App[B firecore.Block](chain *firecore.Chain[B], root
 				blockType = chain.DefaultBlockType
 			}
 
-			blockTypeFromFlag := viper.GetString("block-type")
+			blockTypeFromFlag := viper.GetString("substreams-tier1-block-type")
 
 			if blockTypeFromFlag != "" {
 				blockType = blockTypeFromFlag
