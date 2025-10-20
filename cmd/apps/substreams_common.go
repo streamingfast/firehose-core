39525abc9329a74982bb036ec079b306d84e8fb5
Matthew Darwin <matthew@mdarwin.ca>
2025-10-20 18:06:44 -0400

    Update substreams_common.go
    Fix documentation for HTTP header for X-Substreams-Cache-Tag

--- cmd/apps/substreams_common.go
+++ cmd/apps/substreams_common.go
@@ -13,7 +13,7 @@ func registerCommonSubstreamsFlags(cmd *cobra.Command) {
 	registerSSOnce.Do(func() {
 		cmd.Flags().Uint64("substreams-state-bundle-size", uint64(1_000), "Interval in blocks at which to save store snapshots and output caches")
 		cmd.Flags().String("substreams-state-store-url", "{sf-data-dir}/localdata", "where substreams state data are stored")
-		cmd.Flags().String("substreams-state-store-default-tag", "", "If non-empty, will be appended to {substreams-state-store-url} (ex: 'v1'). Can be overriden per-request with 'X-Sf-Substreams-Cache-Tag' header")
+		cmd.Flags().String("substreams-state-store-default-tag", "", "If non-empty, will be appended to {substreams-state-store-url} (ex: 'v1'). Can be overriden per-request with 'X-Substreams-Cache-Tag' header")
 		cmd.Flags().Duration("substreams-block-execution-timeout", 3*time.Minute, "Maximum execution time for a block before the request is canceled")
 	})
 }
